db4c5b3e9b5ee8e4bee6f333b662816475ce6c2b
eunsung ahn <eunsungahn@eunsungui-MacBookPro.local>
2026-09-07 04:13:11 +0900

    Update 260907

--- src/CategorySelector.js
+++ src/CategorySelector.js
@@ -7,7 +7,7 @@ function CategorySelector() {
   const categories = [
     { id: 'kanji', name: '한자' },
     { id: 'japanese', name: '일본어' },
-    { id: 'joayo', name: '좋아요 일본어 (미완)' },
+    { id: 'joayo', name: '좋아요 일본어' },
     { id: 'toeic', name: '해커스 노랭이' },
   ];
 
